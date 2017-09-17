86e9b07c55f1e63a4c108a1c2be14da44e736d7a
high-moctane <high.moctane@gmail.com>
2017-09-17 16:12:48 +0900

    added main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port = flag.String("p", "80", "set port number")
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if _, err := fmt.Fprintf(w, "(｀･ω･´)"); err != nil {
+		log.Println("simple-shakiin-server:", err)
+		return
+	}
+	log.Println("simple-shakiin-server: (｀･ω･´)")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Println("simple-shakiin-server:", err)
+	}
+}
